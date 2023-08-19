f5a7fc46d531f51fb9465b8eedda038d80bf9bc7
simoncct1 <simoncouclet@gmail.com>
2023-08-19 13:32:02 +0200

    testsmap2

--- assets/map/environment-tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="environment-tiles" tilewidth="16" tileheight="16" tilecount="672" columns="42">
- <image source="environment-tiles.png" width="672" height="256"/>
-</tileset>
